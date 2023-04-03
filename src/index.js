06f5d9429d5fd37d281f8ac8d2e30021b467816a
Raquel <r.f.reategui@gmail.com>
2023-04-03 13:32:15 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
-
-console.log(validator);
+document.querySelector("#validate").addEventListener("click", ()=>{
+  const card=document.querySelector("#card")
+  console.log(card.value)
+  if (card.length < 13 || card.length >19) {
+    alert('Revise el número de tarjeta');
+  }
+  if (validator.isValid(card.value)) {
+    const resultMask = validator.maskify(card.value);
+    card.value = resultMask;
+    alert("Tu tarjeta "+resultMask+'. Gracias por tu compra');
+  } else{
+    const resultMask = validator.maskify(card.value);
+    card.value = resultMask;
+    alert('Tarjeta '+resultMask+' inválida');
+  }
+});
